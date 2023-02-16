edc5fc7830ce075047de3aee7db332e307f5b219
Mahmoud Helmy <mahmoud98398@gmail.com>
2023-02-16 05:32:38 -0800

    9 commit 6

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,10 +8,10 @@ int main(void)
 {
 char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 int i = 0;
-for( i = 0; i< (int)sizeof(s); i++)
+for( i = 0; i < ( int)sizeof( s); i++)
 {
 putchar(s[i]);
 }
-putchar('\n');
+//putchar('\n');
 return (1);
 }
