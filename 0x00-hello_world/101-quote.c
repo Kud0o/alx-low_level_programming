01b44003b3a71858c80957966c0e58525d3470de
Mahmoud Helmy <mahmoud98398@gmail.com>
2023-02-16 05:15:52 -0800

    9 commit 1

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+i#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+for(int i =0;i<sizeof(s);i++)
+{
+putchar(s[i]);
+}
+putchar('\n');
+return (0);
+}
