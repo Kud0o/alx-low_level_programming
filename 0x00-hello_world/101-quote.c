e2ca1ae833e227720ccaa97a5c1750101fdf4613
Mahmoud Helmy <mahmoud98398@gmail.com>
2023-02-16 05:55:12 -0800

    9 commit 9

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,18 +6,12 @@
  */
 int main(void)
 {
-char s[] = "and that piece of art is useful";
-char s2 [] = " - Dora Korpar, 2015-10-19";
+char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 int i = 0;
 for( i = 0; i < ( int)sizeof( s); i++)
 {
 putchar( s[i]);
 }
-putchar( '\"');
-for( i = 0; i < ( int)sizeof( s2); i++)
-{
-putchar( s2[i]);
-}
-putchar( '\n');
+
 return ( 1);
 }
