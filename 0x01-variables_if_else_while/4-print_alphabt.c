e535346cbe8e53d4417349233a1f3b18822e04c0
Mahmoud Helmy <mahmoud98398@gmail.com>
2023-02-17 13:29:16 -0800

    4 commit 1

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{	if (a == 'q' || a == 'e')
+		{continue;
+		}
+		putchar(c++);
+	}
+	putchar('\n');
+
+	return (0);
+}
