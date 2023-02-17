7f5ea2a6dc99d04a67f1a94bbc5e6d80c6694710
Mahmoud Helmy <mahmoud98398@gmail.com>
2023-02-17 15:07:34 -0800

    102 commit 1

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int  new = 1;
+	int i = 48;
+	int j = 48;
+	int k = 48;
+	int l = 48;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = 48; j < 58; j++)
+		{
+			for (k = i; k < 58; k++)
+			{
+				for (l = j+1; l < 58; l++)
+				{
+					if (1)
+					{
+					
+						if (new == 0)
+						{
+							putchar(',');
+							putchar(' ');
+						}
+						putchar(i);
+						putchar(j);
+						putchar(' ');
+						putchar(k);
+						putchar(l);
+					}
+				new = 0;
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
