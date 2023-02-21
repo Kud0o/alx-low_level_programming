ad46f0c3ce16729e50d41f66a9f30bb50aeed3e3
Mahmoud Helmy <mahmoud98398@gmail.com>
2023-02-21 13:59:52 -0800

    1 commit 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
