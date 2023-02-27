7888e28dba47b4dbf577a5e029a9256d56ec9989
Mahmoud Helmy <mahmoud98398@gmail.com>
2023-02-27 10:22:53 -0800

    1 commit 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - swaping arguments 
+ * @a: integer pointer
+ * @b: integer pointer
+ * Returs void
+*/
+
+void swap_int(int *a, int *b)
+{
+int s = *a;
+*a = *b;
+*b = s;
+
+}
