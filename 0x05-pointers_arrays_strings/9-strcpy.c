17eb2b285a9c86833085b94e4d92f1dd43c60cb1
Mahmoud Helmy <mahmoud98398@gmail.com>
2023-02-27 15:04:32 -0800

    10 commit 1

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strcpy - printing numbers
+ * @s: char pointer
+ * @a: int
+ * Return: void
+*/
+
+void _strcpy(char *s, char * a)
+{
+int i = 0;
+strcpy(s, a);
+
+}
