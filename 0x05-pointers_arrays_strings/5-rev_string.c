6a96711e9c0e371641bd7a78152d4994de89ef5c
Mahmoud Helmy <mahmoud98398@gmail.com>
2023-02-27 12:28:53 -0800

    6 commit 1

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rev_string - make string reversed
+ * @s: char pointer
+ * Return: void
+*/
+
+void rev_string(char *s)
+{
+int c = 0;
+int i = 0;
+char *ss = s;
+while (1)
+{
+	if (s[i++] == '\0')
+		{
+		break;
+		}
+	c++;
+}
+
+for (i = 0; i < c; i++)
+	{
+	ss[i] = s[c - 1 - i]);
+	}
+
+}
