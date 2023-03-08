92ef38b77aac3650cbb9464c174ec3484806035b
Mahmoud Helmy <mahmoud98398@gmail.com>
2023-03-08 15:47:52 -0800

    last commit 1

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,19 +1,6 @@
 #include "main.h"
 
 /**
- * _pow_recursion - raises x to the power of y
- * @x: Number Integer
- * @y: Power
- *
- * Return: On success 1.
- * Base case  -> if y less thN 0.
- */
-int is_prime_number(int n)
-{
-	return (_is_prime_helper(n, n - 1));
-}
-
-/**
  * _is_prime_helper - returns the natural square root of a number.
  * @n: input number.
  * @x: current number.
@@ -27,5 +14,15 @@ int _is_prime_helper(int n, int x)
 		return (1 && _is_prime_helper(n, x - 1));
 	if (n % x  == 0)
 		return (0);
+}
+
+/**
+ * is_prime_number - raises x to the power of y
+ * @n: Number Integer
+ * Return: int
+ */
+int is_prime_number(int n)
+{
+        return _is_prime_helper(n, n - 1);
 }
 
